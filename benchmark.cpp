7a8546345a1d6de48c7141d4cbcbfb906f6742c0
= <=>
2026-09-22 14:35:34 -0700

    Added print statement for the elapsed time with problem size

--- benchmark.cpp
+++ benchmark.cpp
@@ -107,6 +107,9 @@ int main(int argc, char** argv)
            std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
            std::chrono::duration<double> elapsed = end_time - start_time;
 
+
+           printf("Problem size: %d, Time: %f seconds\n", n, elapsed.count());
+
            reference_dgemm(n, 1.0 , Acopy, Bcopy, Ccopy);
 
            // compare your C with that computed by BLAS
